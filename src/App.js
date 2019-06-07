fd2a7c4d632fb1631227ee136680318828bba1c4
Elnur Sadigov <elnur.sadigov.uk@gmail.com>
2019-06-07 18:39:47 +0100

    Added /orders route to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <React.Fragment>
             <Layout>
                 <Switch>
+                    <Route path="/orders" component={Orders} />
                     <Route path="/checkout" component={Checkout} />
                     <Route path="/" exact component={BurgerBuilder} />
                 </Switch>

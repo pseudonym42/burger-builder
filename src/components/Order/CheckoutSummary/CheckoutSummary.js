fcfe025c1f62982f9fb1159919cb716216ab3e5b
Elnur Sadigov <elnur.sadigov.uk@gmail.com>
2019-06-07 18:43:58 +0100

    Added actions to buttons on CheckoutSummary.js component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -7,14 +7,14 @@ import styles from './CheckoutSummary.module.css';
 const checkoutSummary = (props) => {
     return (
         <div className={styles.CheckoutSummary}>
-            <h1>Enjoy!</h1>
+            <h1>Proceed to checkout?</h1>
             <div style={{
                 width: '100%',
                 margin: 'auto'
             }}>
                 <Burger ingredients={props.ingredients}/>
-                <Button buttonType="Danger" action>CANCEL</Button>
-                <Button buttonType="Success" action>CONFIRM</Button>
+                <Button buttonType="Danger" action={props.cancelCheckout}>CANCEL</Button>
+                <Button buttonType="Success" action={props.confirmCheckout}>CHECKOUT</Button>
             </div>
         </div>
     )
